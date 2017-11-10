86e71737af8a654bb243b1b0f0b892ad5623c970
Rock de Vocht <peter@peter.co.nz>
2017-11-11 09:59:04 +1300

    commented out broken Bing search

--- src/main/java/info/ephyra/OpenEphyra.java
+++ src/main/java/info/ephyra/OpenEphyra.java
@@ -288,7 +288,7 @@ public class OpenEphyra
         // search
         // - knowledge miners for unstructured knowledge sources
         Search.clearKnowledgeMiners();
-        //Search.addKnowledgeMiner(new BingAzureKM());
+        //Search.addKnowledgeMiner(new BingAzureKM());  // todo: broken
 
         // for (String[] indriIndices : IndriKM.getIndriIndices())
         // Search.addKnowledgeMiner(new IndriKM(indriIndices, false));
